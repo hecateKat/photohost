11428ce1693a8908bcfc8c155a17a45a05fd8050
Kat <kat.ochmanska@gmail.com>
2020-02-03 13:02:56 +0100

    update configuration after circle dependency injection error

--- src/main/java/com/kat/photohost/security/WebSecurityConfig.java
+++ src/main/java/com/kat/photohost/security/WebSecurityConfig.java
@@ -1,25 +1,36 @@
 package com.kat.photohost.security;
 
+import com.kat.photohost.entity.User;
+import com.kat.photohost.impl.UserDetailsServiceImpl;
+import com.kat.photohost.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.event.EventListener;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.User;
+
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-
-import java.util.Collection;
-import java.util.Collections;
 
 @Configuration
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
+    private UserDetailsServiceImpl userDetailsServiceimpl;
+    private UserRepository userRepository;
+
+    @Autowired
+    public WebSecurityConfig(UserDetailsServiceImpl userDetailsServiceimpl, UserRepository userRepository) {
+        this.userDetailsServiceimpl = userDetailsServiceimpl;
+        this.userRepository = userRepository;
+    }
+
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication().withUser(
-                new User("Kat", passwordEncoder().encode("abc123"), Collections.singleton(new SimpleGrantedAuthority("user"))));
+        auth.userDetailsService(userDetailsServiceimpl);
+
     }
 
     @Override
@@ -35,5 +46,12 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     public PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
     }
+
+    @EventListener(ApplicationReadyEvent.class)
+    public void TestUser(){
+        User user = new User("Kat", passwordEncoder().encode("abc123"), "USER");
+        userRepository.save(user);
+    }
+
 
 }
